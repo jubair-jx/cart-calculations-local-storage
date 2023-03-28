c1cd2aac97b89daa006b87359ac642eead44d1fe
Mohammad Jubair <you@example.com>
2023-03-28 15:02:16 +0600

    Data Add on local Storage

--- src/components/utilities/fakeDB.jsx
+++ src/components/utilities/fakeDB.jsx
@@ -1,14 +1,19 @@
 const addToCart = (id) => {
-  const localData = localStorage.getItem(id);
-
-  if (localData) {
-    let ShoopingCart = {};
-    localStorage.setItem("shopping-cart", JSON.stringify(ShoopingCart));
-
-    const Newqunatity = parseInt(localData) + 1;
-    localStorage.setItem(id, Newqunatity);
+  let ShoopingCart;
+  let storeCart = localStorage.getItem("shopping-cart");
+  if (storeCart) {
+    ShoopingCart = JSON.parse(storeCart);
   } else {
-    localStorage.setItem(id, 1);
+    ShoopingCart = {};
   }
+  const localData = ShoopingCart[id];
+  if (localData) {
+    const Newqunatity = localData + 1;
+    ShoopingCart[id] = Newqunatity;
+    // localStorage.setItem(id, Newqunatity);
+  } else {
+    ShoopingCart[id] = 1;
+  }
+  localStorage.setItem("shopping-cart", JSON.stringify(ShoopingCart));
 };
 export { addToCart };
